a46c559f7c181a032027515476260672ac426ffa
Peter Corcoran <petercorcoran@Peters-MacBook-Pro.local>
2026-05-07 10:00:52 +0100

    Initial commit

---
+++ src/main/java/com/ericsson/festivalpulse/FestivalPulseApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.festivalpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestivalPulseApplication.class, args);
+    }
+
+}
